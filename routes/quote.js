3f65e775e9c12611675ee4430bff9336c8ee4f11
kiruthikx <kiruthikx@gmail.com>
2024-10-03 17:07:46 +0000

    lol

--- routes/quote.js
+++ routes/quote.js
@@ -21,12 +21,44 @@ apiInit();
 
 // Define the /random route
 router.get('/random', (req, res) => {
-    let rdm = genRandom();
-    res.json({ quote: data[rdm].quote, author: data[rdm].author,category :data[rdm].category,id:rdm });
-});
+apiInit();
 
+    let rdm = genRandom();
+    let qbj = data[rdm]
+    let minLength = parseInt(req.query.minLength) || null
+    let maxLength = parseInt(req.query.maxLength) || null
+    //    if(minLength){
+    //     console.log(qbj)
+    //     while(qbj.quote.length < minLength){
+    //         rdm=genRandom();
+    //     }
+    //    }
+    //    if(maxLength){
+    //        while(qbj.quote.length > maxLength){
+    //            rdm=genRandom();
+    //            console.log(qbj)
+    //     }
+    //    } 
+        
+        console.log(qbj)
+    res.json({min:minLength,max:maxLength, quote: data[rdm].quote, author: data[rdm].author,category :data[rdm].category,id:rdm });
+});
+router.get('/',(req,res)=>{
+    
+
+
+})
 router.get('/:id',(req,res)=>{
+apiInit();
+
     let rdm = req.params.id;
     res.json({ quote: data[rdm].quote, author: data[rdm].author,category :data[rdm].category,id:rdm });
+})
+
+
+router.get('/categories',(req,res)=>{
+    res.json({
+        categories : ['life', 'happiness', 'love', 'truth', 'inspiration', 'humor', 'philosophy', 'science', '', 'soul', 'books', 'wisdom', 'knowledge', 'education', 'poetry', 'hope', 'friendship', 'writing', 'religion', 'death', 'romance', 'success', 'arts', 'relationship', 'motivation', 'faith', 'mind', 'god', 'funny', 'quotes', 'positive', 'purpose']
+    })
 })
 export default router;
