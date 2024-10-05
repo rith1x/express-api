18baef701a9bcb3085d47b235692256e78732b0b
kiruthikx <kiruthikx@gmail.com>
2024-10-05 01:16:43 +0000

    lpl

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 import express from 'express';
+const cors = require('cors');
 import quote from './routes/quote.js'; // Ensure .js extension
-
 const app = express();
 
+const allowedOrigins = ['https://rith1x.github.io', 'http://127.0.0.1:5501'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 // Basic route
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
